43a899db137c85d1cc70067c1e748f1f6dc95be1
hoang_anh <ledinhhoanganh10@gmail.com>
2024-05-05 18:18:57 +0700

    authentication & theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      This is a protected route.
+      <UserButton 
+      afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
    
   );
