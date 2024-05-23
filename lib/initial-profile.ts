4807dc71cff9c15153b771493226e5cbcdff03ab
hoang_anh <ledinhhoanganh10@gmail.com>
2024-05-24 04:38:21 +0700

    fix bug

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,5 +1,4 @@
 import { currentUser, auth } from "@clerk/nextjs/server";
-import { redirectToSignIn } from "@clerk/nextjs/server";
 import { db} from "@/lib/db";
 
 
@@ -7,7 +6,7 @@ export const initialProfile = async () => {
 
     const user = await currentUser();
     if (!user) {
-        return  redirectToSignIn();
+        return  auth().redirectToSignIn();
     }
     
     const profile = await db.profile.findUnique({
