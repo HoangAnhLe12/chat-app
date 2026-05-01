3f84cd4f69b0ad28597833d0f4fb86a0bbd90c3d
Lê Đình Hoàng Anh <ledinhhoanganh10@gmail.com>
2026-05-01 23:12:50 +0700

    fix: update ui

--- app/(main)/(routes)/user/[id]/layout.tsx
+++ app/(main)/(routes)/user/[id]/layout.tsx
@@ -1,34 +1,29 @@
-import { auth} from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 
 import { currentProfile } from "@/lib/current-profile";
 import { UserSidebar } from "@/components/user/user-sidebar";
 
 const UserIdLayout = async ({
-    children,
-    params,
-}:{
-    children: React.ReactNode;
-    params: {userId: string};
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { userId: string };
 }) => {
+  const profile = await currentProfile();
 
-    const profile = await currentProfile();
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
 
-    if(!profile) {
-        return auth().redirectToSignIn();
-    }
-
-    return (
-        <div className="h-full">
-            <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-                <UserSidebar
-                id={params.userId}
-                />
-            </div>
-            <main className="h-full md:pl-60">
-                {children}
-            </main>
-        </div>
-    );
-}
+  return (
+    <div className="h-full">
+      <div className="md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <UserSidebar id={params.userId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+};
 
 export default UserIdLayout;
